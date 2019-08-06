5db547777f6e477f43af94bebdb3ffe5c6f159dc
Puneeth Reddy H C <puneethreddy951@gmail.com>
2019-08-06 11:14:09 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "APPLICATION_PORT": 9000,
+    "TOKEN_SECRET": "!@project_name@!",
+    "TOKEN_EXPIRY": 525600,
+    "ALERT_EMAIL": "mail@avohi.com",
+    "VERSION_V1_URL": '/project_name/api/v1/',
+    "SMS_CAMPAIGN_NAME": "project_name",
+    "SMS_SENDER_NAME": "project_name",
+    "SENDER_EMAIL": "puneethreddy951@gmail.com",
+    "EMAIL_PASSWORD": "GI2DENRTGQ3DILRX",
+    "GMAIL_PASSWORD":"GI2DENRTGQ3DILRX"
+}
---
+++ db.js
@@ -0,0 +1,6 @@
+module.exports = {
+ 
+     'url': "mongodb://localhost:27017/project_name",
+    //'url': 'mongodb://studentplatform:student_2018@cluster0-shard-00-00-fa0px.gcp.mongodb.net:27017,cluster0-shard-00-01-fa0px.gcp.mongodb.net:27017,cluster0-shard-00-02-fa0px.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+
+};
